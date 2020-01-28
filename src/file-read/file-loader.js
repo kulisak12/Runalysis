e1ec0740e5711c136621269e723a3610f8678663
kulisak12 <kulisak12lp@gmail.com>
2020-01-28 11:02:54 +0100

    Added sample gpx file

--- src/file-read/file-loader.js
+++ src/file-read/file-loader.js
@@ -15,6 +15,23 @@ function getFile() {
 	}
 }
 
+function sampleFile() {
+	var file = new XMLHttpRequest();
+    file.onreadystatechange = function () {
+		if(file.readyState === 4) {
+			if(file.status === 200 || file.status == 0){
+				var fileText = file.responseText;
+				var run = parser(fileText);
+				sessionStorage.setItem("runData", JSON.stringify(run));
+				
+				document.getElementById("error").innerHTML = "";
+				window.location.href = "view.html";
+            }
+        }
+    }
+	file.open("GET", "/bezzastaveni.gpx", false);
+    file.send(null);
+}
 
 // extract content
 function readFile(file, parser) {
